32d864145bf5be2a74ce06f765fa86758817baac
Richard <programmingislife23@gmail.com>
2024-02-21 15:18:30 -0300

    jogo finalizado na branch de testes

--- app.js
+++ app.js
@@ -9,3 +9,47 @@ let display = [];
 for (let i = 0; i != word.length; i++) {
   display.push("_");
 }
+
+let tentativasDeLetras = [];
+let vidas = 6;
+let gameOn = true;
+console.log(`Palavra secreta: ${display}.`);
+console.log(`Suas vidas: ${vidas}.`);
+
+while (gameOn == true) {
+  let letraTentada = prompt(
+    "Tente adivinhar uma letra da palavra secreta.".toLowerCase()
+  );
+
+  if (
+    display.includes(letraTentada) ||
+    tentativasDeLetras.includes(letraTentada)
+  ) {
+    console.log(`Você já tentou essa letra: ${letraTentada}.`);
+  }
+
+  tentativasDeLetras.push(letraTentada);
+
+  for (let i = 0; i < word.length; i++) {
+    letra = word[i];
+    if (letraTentada == letra) {
+      display[i] = letra;
+      console.log(`Palavra secreta: ${display}.`);
+    }
+  }
+  if (!display.includes(letraTentada)) {
+    vidas -= 1;
+    console.log(`Suas vidas: ${vidas}.`);
+  }
+  if (!display.includes("_")) {
+    console.log(
+      `Parabéns! Você venceu o jogo. Descobrindo a palavra secreta: ${word}.`
+    );
+    gameOn = false;
+  } else if (vidas == 0) {
+    console.log(
+      `Infelizmente você perdeu o jogo, não adivinhando a palavra secreta: ${word}.`
+    );
+    gameOn = false;
+  }
+}
