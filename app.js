4a766c12e1ae21699c1fd514e0625db1306e92bb
Richard <programmingislife23@gmail.com>
2024-02-21 14:47:17 -0300

    implementando o display, para mostrar o número de tracinhos que tem na palavra secreta.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/* selecionando uma palavra aleotória */
 const wordList = ["mouse", "teclado", "tv"];
 const randomNumber = Math.floor(Math.random() * wordList.length);
 const word = wordList[randomNumber];
+/* display para mostrar os tracinhos da palavra secreta
+for loop para adicionar os tracinhos de acordo com o número de letras na palavra.
+*/
+let display = [];
+for (let i = 0; i != word.length; i++) {
+  display.push("_");
+}
